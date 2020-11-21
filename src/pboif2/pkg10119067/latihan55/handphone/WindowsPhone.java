dbda15f40c600bcad67fb3a60cdab7df44579176
User <User@DESKTOP-6N6EHH6>
2020-11-21 22:44:52 +0700

    PBOIF2-10119067

---
+++ src/pboif2/pkg10119067/latihan55/handphone/WindowsPhone.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119067.Latihan55.handphone;
+
+/**
+ *
+ * @author Danhago
+ * NAMA     : Andreas Danhago P.I
+ * KELAS    : IF2
+ * NIM      : 10119067
+ * Deskripsi: Handphone
+ */
+public class WindowsPhone extends Handphone{
+    private String wpKeyStore;
+
+    public WindowsPhone(String man, String os, String model, int harga) {
+        super(man, os, model, harga);
+    }
+
+    public String getWpKeyStore() {
+        return wpKeyStore;
+    }
+
+    public void setWpKeyStore(String wpKeyStore) {
+        this.wpKeyStore = wpKeyStore;
+    }
+    
+    
+}
